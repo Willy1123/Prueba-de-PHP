6315b97015c355d993a22a938c0f9542099e8027
guillermoblascoperez@gmail.com <>
2016-09-19 18:35:00 +0200

    con fondo

--- index.php
+++ index.php
@@ -5,31 +5,35 @@
         <meta charset="UTF-8">
         <title>Prueba de PHP</title>
         <link rel="stylesheet" href="css/bootstrap.min.css">
+        <style>
+            body {
+                background: black;
+                background-image: url("img/tile2.png");
+            }
+        </style>
     </head>
     <body>
         <div class="container">
             <div class="row">
-                <div class="col-md-12 text-center"><h2>EJEMPLO DE INICIO DE SESIÓN EN PHP</h2></div>
+                 <div class="col-md-12"><h2 class="text-center" style="color:white;">EJEMPLO DE INICIO DE SESIÓN EN PHP</h2></div>
             </div>
             <div class="row">
                 <div class="col-md-4"></div>
                 <div class="col-md-4">
                     <form action="index2.php" method="post">
                         <br>
-                        <h3>Nombre:</h3>
                         <input name="usuario_nombre" class="form-control" type="text" placeholder="Usuario">
-                        
-                        <h3>Contraseña:</h3>
-                        <input name="usuario_clave" class="form-control" type="password" placeholder="Contraseña">
                         <br>
-                        <button class="btn btn-success btn-block" type="submit">Entrar</button>
+                        <input name="usuario_clave"  class="form-control" type="password" placeholder="Contraseña">
+                        <br>
+                        <button class="btn btn-success btn-block" type="submit"> Entrar</button>
                     </form>
                 </div>
                 <div class="col-md-4"></div>
             </div>
         </div>
         <?php
-        
+          
         ?>
     </body>
     <script src="js/jquery-3.1.0.min.js"></script>
