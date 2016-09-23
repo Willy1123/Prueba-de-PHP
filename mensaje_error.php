02d0ffab6e15c806135a2ad57f53a4c0fff23616
guillermoblascoperez@gmail.com <xp@DESKTOP-7PVH85D>
2016-09-23 17:39:29 +0200

    Con el menu de inicio dividido correctamente

---
+++ mensaje_error.php
@@ -0,0 +1,6 @@
+
+<nav class="navbar navbar-static-top" role="navigation">
+    <button class="btn" style="background-color: #269abc; color:white; padding: 15px;">
+        Bienvenido a la Web de Prácticas de Histología
+    </button>
+</nav>
